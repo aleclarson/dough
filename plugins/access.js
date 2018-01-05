64303255fd7c1a439907f70376ebe27285799740
aleclarson <alec.stanford.larson@gmail.com>
2018-01-05 17:45:07 -0500

    rewrite `attr` method

--- plugins/access.js
+++ plugins/access.js
@@ -1,55 +1,46 @@
 
 const u = require('./plugins/core');
+const isObject = require('is-object');
 
-// [INTERNAL USE ONLY]
-
-// Handle attributes for the matched elements
-u.prototype.attr = function (name, value, data) {
-  data = data ? 'data-' : '';
-
-  // This will handle those elements that can accept a pair with these footprints:
-  // .attr('a'), .attr('a', 'b'), .attr({ a: 'b' })
-  return this.pairs(name, value, function (node, name) {
-    return node.getAttribute(data + name);
-  }, function (node, name, value) {
-    node.setAttribute(data + name, value);
-  });
+u.prototype.attr = function () {
+  const {nodes} = this
+  let name = arguments[0]
+  let value = arguments[1]
+  if (typeof name == 'string') {
+    if (arguments.length == 1) {
+      if (nodes.length == 0) return
+      return nodes[0].getAttribute(name)
+    }
+    if (nodes.length && typeof value != 'undefined') {
+      setAttribute(nodes, name, value)
+    }
+  } else if (nodes.length && isObject(name)) {
+    const values = name
+    for (name in values) {
+      value = values[name]
+      setAttribute(nodes, name, value)
+    }
+  }
+  return this
 };
-
 
 // Handle data-* attributes for the matched elements
 u.prototype.data = function (name, value) {
-  return this.attr(name, value, true);
+  return this.attr('data-' + name, value);
 };
-
-
-// [INTERNAL USE ONLY]
-
-// Take the arguments and a couple of callback to handle the getter/setter pairs
-// such as: .css('a'), .css('a', 'b'), .css({ a: 'b' })
-u.prototype.pairs = function (name, value, get, set) {
-  // Convert it into a plain object if it is not
-  if (typeof value !== 'undefined') {
-    var nm = name;
-    name = {};
-    name[nm] = value;
-  }
-
-  if (typeof name === 'object') {
-    // Set the value of each one, for each of the { prop: value } pairs
-    return this.each(function (node) {
-      for (var key in name) {
-        set(node, key, name[key]);
-      }
-    });
-  }
-
-  // Return the style of the first one
-  return this.length ? get(this.first(), name) : '';
-};
-
 
 // Find the size of the first matched element
 u.prototype.size = function () {
   return this.first().getBoundingClientRect();
 };
+
+//
+// Helpers
+//
+
+function setAttribute(nodes, name, value) {
+  for (let i = 0; i < nodes.length; i++) {
+    nodes[i].setAttribute(name, value)
+  }
+}
+
