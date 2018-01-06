abde84badad0fbcb07b4bfc8f2b05978e9c248ee
aleclarson <alec.stanford.larson@gmail.com>
2018-01-06 18:17:07 -0500

    remove `replace` method

--- plugins/content.js
+++ plugins/content.js
@@ -73,16 +73,6 @@ u.prototype.remove = function() {
   return this.each(removeNode)
 };
 
-// Replace the matched elements with the passed argument.
-u.prototype.replace = function (html, data) {
-  var nodes = [];
-  this.adjacent(html, data, function (node, fragment) {
-    nodes = nodes.concat(this.slice(fragment.children));
-    node.parentNode.replaceChild(fragment, node);
-  });
-  return u(nodes);
-};
-
 u.prototype.text = function(text) {
   if (arguments.length) {
     const {nodes} = this
