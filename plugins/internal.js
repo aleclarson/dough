06b965d80140b864d577a9ba52bb61fbab487612
aleclarson <alec.stanford.larson@gmail.com>
2018-01-06 18:17:42 -0500

    remove `generate` internal method

--- plugins/internal.js
+++ plugins/internal.js
@@ -40,20 +40,6 @@ u.prototype.eacharg = function(args, iterator) {
 }
 
 
-// [INTERNAL USE ONLY]
-// Generate a fragment of HTML. This irons out the inconsistences
-u.prototype.generate = function (html) {
-  // Table elements need to be child of <table> for some f***ed up reason
-  if (/^\s*<t(h|r|d)/.test(html)) {
-    return u(document.createElement('table')).html(html).children().nodes;
-  } else if (/^\s*</.test(html)) {
-    return u(document.createElement('div')).html(html).children().nodes;
-  } else {
-    return document.createTextNode(html);
-  }
-};
-
-
 /**
  * Internal use only. This function checks to see if an element is in the page's body. As contains is inclusive and determining if the body contains itself isn't the intention of isInPage this case explicitly returns false.
 https://developer.mozilla.org/en-US/docs/Web/API/Node/contains
