44469d5fa1b344837cca73c16088780651ef472a
aleclarson <alec.stanford.larson@gmail.com>
2018-01-06 12:49:05 -0500

    rewrite the `eacharg` method

--- plugins/internal.js
+++ plugins/internal.js
@@ -55,17 +55,17 @@ function splitString(str) {
 }
 
 // [INTERNAL USE ONLY]
-// Loop through the combination of every node and every argument passed
-u.prototype.eacharg = function (args, callback) {
-  return this.each(function (node, i) {
-    this.args(args, node, i).forEach(function (arg) {
-      // Perform the callback for this node
-      // By doing callback.call we allow "this" to be the context for
-      // the callback (see http://stackoverflow.com/q/4065353 precisely)
-      callback.call(this, node, arg);
-    }, this);
-  });
-};
+u.prototype.eacharg = function(args, iterator) {
+  const {nodes} = this
+  if (nodes.length) {
+    this.args(args).forEach(arg => {
+      for (let i = 0; i < nodes.length; i++) {
+        iterator.call(this, nodes[i], arg)
+      }
+    })
+  }
+  return this
+}
 
 
 // [INTERNAL USE ONLY]
