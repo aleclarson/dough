d89981b303855e4cfb67948932c460c7645cfac5
aleclarson <alec.stanford.larson@gmail.com>
2018-01-14 18:10:37 -0500

    remove `u.ready()`
    Prefer placing <script> tags at the end of the <body> tag instead.

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ require('./plugins/content');
 require('./plugins/css');
 require('./plugins/events');
 require('./plugins/internal');
-require('./plugins/ready');
 require('./plugins/scroll');
 require('./plugins/traversal');
 require('./plugins/tween');
